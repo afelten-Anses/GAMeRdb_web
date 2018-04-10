9d55976fcd1064b3e7bfbcd559e1b8584fe68ac9
KDurimel <k.durimel@gmail.com>
2018-04-10 18:30:46 +0200

    reimplemented basic compression

--- GAMeRdb_dev/scripts/ZipAndCall.sh
+++ GAMeRdb_dev/scripts/ZipAndCall.sh
@@ -3,6 +3,9 @@
 #Arg 1 : directory uuid
 #Arg 2 : list of files to zip
 
+#
+### En prod : add -q option 
+#
 
 # init and create currentir : zip directory (tmp/uuid/)
 currentDir=tmp/$1 #arg $1 = uuid
@@ -11,7 +14,10 @@ mkdir -p $currentDir
 # parse list of files to zip 
 filesList=$2 #$2 = file path : tmp/uuid/filename
 
-# Zip files (listed in fileslist) with the lowest compression ratio (0)
-zip -0 $currentDir/wgsdata -@ < $filesList
+# Zip files (listed in fileslist) with the lowest compression ratio (0).
+
+currentDate=$(date +"%Y%m%d_%Hh%Mm%Ss")
+zipName="wgsdata-"$currentDate
+zip -0 $currentDir/$zipName -@ < $filesList
 
 #Then, zip file is availaible until they are deleted (weekly)
