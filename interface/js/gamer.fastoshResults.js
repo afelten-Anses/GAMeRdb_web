ebd34286d879bf9af0de314e2d439d510f4c0b91
pbarbet <barbet.pauline@orange.fr>
2018-07-25 11:37:27 +0200

    Add circular and rectangular tree transitions

--- interface/js/gamer.fastoshResults.js
+++ interface/js/gamer.fastoshResults.js
@@ -14,22 +14,23 @@ $(document).ready(function() {
             1000,
             'circular'
         );
-        console.log(dataNewick)
-        phylocanvas2 = new Smits.PhyloCanvas(
+       phylocanvas2 = new Smits.PhyloCanvas(
             dataObject,
             'svgCanvasRect',
             1000,
             1500,
             'rectangular'
         );
+
+        console.log(dataNewick)
         var svgSource = phylocanvas.getSvgSource() // svg code which will be downloadable as a blob file
         console.log(svgSource)
     });
-    
+    $('a[href="distance_matrix"]').attr('href',"../tmp/fastosh_" + uuid + "/distance_matrix.tsv")
+    $('a[href="newick"]').attr('href',"../tmp/fastosh_" + uuid + "/taxonomy.nwk")
 });
 
-$('a[href="distance_matrix"]').attr('href',"../tmp/fastosh_" + uuid + "/distance_matrix.tsv")
-$('a[href="newick"]').attr('href',"../tmp/fastosh_" + uuid + "/taxonomy.nwk")
+
 
 $("#helpme").click(function()
 {
@@ -38,8 +39,27 @@ $("#helpme").click(function()
   }).modal('show');
 });
 
-$("#switchtree").click(function() {
-    /*$('#svgCanvas').transition('fade');*/
-    $('#svgCanvasRect').show();
+$("#switchtree").on("click", function() {
+    //$('#svgCanvas').hide();
+    //$('#svgCanvasRect').show();
+    $('#svgCanvas').transition({animation : 'fly right', duration  : 1800});
+    sleep(1800).then ( () =>  {
+        $('#svgCanvasRect').transition({animation : 'fly left', duration  : 1800});
+    });
+    $('#switchtreeRect').show();
+    $('#switchtree').hide();
 });
+
+$("#switchtreeRect").on("click", function() {
+    //$('#svgCanvasRect').hide();
+    //$('#svgCanvas').show();
+    $('#svgCanvasRect').transition({animation : 'fly right', duration  : 1800});
+    sleep(1800).then ( () => {
+        $('#svgCanvas').transition({animation : 'fly left', duration  : 1800});
+    });
+    $('#switchtreeRect').hide();
+    $('#switchtree').show();
+});
+
+
 
