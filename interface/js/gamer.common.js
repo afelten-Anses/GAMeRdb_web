3e20c35b86e4266ca29c75d6354fd943f7135dc6
KDurimel <k.durimel@gmail.com>
2018-07-25 13:56:44 +0200

    sleep function (async)

--- interface/js/gamer.common.js
+++ interface/js/gamer.common.js
@@ -51,10 +51,16 @@ switch(true){
 //              Other JS functions                //
 //------------------------------------------------//
 
-//Generate uuids
+// Generate uuids
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
+
+// Async sleep
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}	
